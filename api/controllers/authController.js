e4c767ce6866266bd85cae647ccb3bf9de04bfa2
BrunoRodrigoT <brunorodrigoteixeira2021@gmail.com>
2023-11-06 15:41:31 -0300

    FEAT-authControler

---
+++ api/controllers/authController.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const router = express.Router();
+
+// Registro
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await argon2.hash(password);
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: "Registration failed" });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const isPasswordValid = await argon2.verify(user.password, password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid password" });
+  }
+
+  const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+    expiresIn: "1h",
+  });
+  res.json({ token });
+});
+
+// Rota protegida
+router.get("/protected", (req, res) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  jwt.verify(token, "your-secret-key", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    res.json({ message: "Welcome to the protected route!", user: decoded });
+  });
+});
+
+module.exports = router;
